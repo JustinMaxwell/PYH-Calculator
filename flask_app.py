843bb005f461109355d1eaa4879487bd55002d5a
Justin Maxwell <jmaxx93@gmail.com>
2019-01-15 13:13:22 -0500

    got user input from form data working

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,18 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def renderMain():
-    return render_template('main.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+@app.route('/', methods=['GET', 'POST'])
+def mainPage():
+    if request.method == 'POST':
+      hours_worked = request.form.get('hours_worked')
+      annual_target = request.form.get('annual_target')
+      hours_per_day = request.form.get('hours_per_day')
+      return render_template('main.html', hours_worked=hours_worked, annual_target=annual_target, hours_per_day=hours_per_day)
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
