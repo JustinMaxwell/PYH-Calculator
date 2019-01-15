788656c0166fa11acd25ac59ab809e9d04c7cf68
Justin Maxwell <jmaxx93@gmail.com>
2019-01-15 15:03:42 -0500

    fixed the pip3 free (must always be in virtualenv) added some pandas dates object to flask_app.py script to start adding logic.

--- flask_app.py
+++ flask_app.py
@@ -2,16 +2,27 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template, request
+import pandas as pd
+from pandas.tseries.holiday import USFederalHolidayCalendar
+from pandas.tseries.offsets import CustomBusinessDay
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def mainPage():
+    today = pd.to_datetime('today')
+    yearEnd = '2019-12-31'
+    us_bd = CustomBusinessDay(calendar=USFederalHolidayCalendar())
+    remainingBDays = pd.DatetimeIndex(start=today,end=yearEnd, freq=us_bd))
+    hours_worked = None
+    annual_target = None
+    hours_per_day = None
     if request.method == 'POST':
-      hours_worked = request.form.get('hours_worked')
-      annual_target = request.form.get('annual_target')
-      hours_per_day = request.form.get('hours_per_day')
-      return render_template('main.html', hours_worked=hours_worked, annual_target=annual_target, hours_per_day=hours_per_day)
+        hours_worked = request.form.get('hours_worked')
+        annual_target = request.form.get('annual_target')
+        hours_per_day = request.form.get('hours_per_day')
+        return render_template('main.html', hours_worked=hours_worked,
+             annual_target=annual_target, hours_per_day=hours_per_day)
     return render_template('main.html')
 
 if __name__ == '__main__':
---
+++ holidays.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from pandas.tseries.offsets import YearEnd, YearBegin, BDay, QuarterEnd, QuarterBegin
+
+today = pd.to_datetime('today')
+
+pd.date_range('2011-01-05', '2011-01-09', freq=BDay())
+
+from pandas.tseries.holiday import USFederalHolidayCalendar
+from pandas.tseries.offsets import CustomBusinessDay
+
+us_bd = CustomBusinessDay(calendar=USFederalHolidayCalendar())
+print(pd.DatetimeIndex(start=today,end='2019-12-31', freq=us_bd))
+
