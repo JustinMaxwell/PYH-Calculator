1e9c339e1c1aa29c48fa306e2a3249b6a988d5b0
Justin Maxwell <jmaxx93@gmail.com>
2019-01-17 20:37:00 -0500

    adding checkbox logic

--- flask_app.py
+++ flask_app.py
@@ -19,10 +19,11 @@ days_off = ''
 
 @app.route('/', methods=['GET', 'POST'])
 def mainPage():
-    blankPage = render_template('main.html', days_off='', holidays='', workable_days='')
+    blankPage = render_template('main.html', days_off='', holidays='', workable_days='', checked=None)
     hours_worked = request.form.get('hours_worked') or request.cookies.get('hours_worked')
     annual_target = request.form.get('annual_target') or request.cookies.get('annual_target')
     hours_per_day = request.form.get('hours_per_day') or request.cookies.get('hours_per_day')
+    include_today = request.form.get('include_today')
     if request.method == 'POST' and hours_worked and annual_target and hours_per_day:
         try:
             hours_worked = float(hours_worked)
@@ -31,11 +32,11 @@ def mainPage():
         except:
             return blankPage
         pyhLeft = annual_target - hours_worked
-        daysLeft = pyhLeft / hours_per_day
+        daysLeft = (pyhLeft / hours_per_day) - int(bool(include_today))
         days_off = round(Decimal(remainingBDays - daysLeft), 2)
         resp = make_response(render_template('main.html', days_off=days_off,
              holidays=holidays, workable_days=workable_days, hours_worked=hours_worked,
-             annual_target=annual_target, hours_per_day=hours_per_day))
+             annual_target=annual_target, hours_per_day=hours_per_day, checked=include_today))
         resp.set_cookie('hours_worked', str(hours_worked))
         resp.set_cookie('annual_target', str(annual_target))
         resp.set_cookie('hours_per_day', str(hours_per_day))
