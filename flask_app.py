3b1fd52f2a19f65f781f5d024dd48950b6500f06
Justin Maxwell <jmaxx93@gmail.com>
2019-01-22 20:29:00 -0500

    added default values for annual minumum and hours per day

--- flask_app.py
+++ flask_app.py
@@ -4,26 +4,28 @@
 from flask import Flask, render_template, request, make_response
 import pandas as pd
 from pandas.tseries.holiday import USFederalHolidayCalendar
-from pandas.tseries.offsets import CustomBusinessDay, YearEnd
+from pandas.tseries.offsets import CustomBusinessDay, YearEnd, YearBegin
 from decimal import Decimal
 
 app = Flask(__name__)
 
 today = pd.to_datetime('today')
+yearBegin = today-YearBegin()
 yearEnd = today+YearEnd()
 us_bd = CustomBusinessDay(calendar=USFederalHolidayCalendar())
-holidays = len(USFederalHolidayCalendar().holidays(start=today, end=yearEnd))
+holidays = USFederalHolidayCalendar().holidays(start=today, end=yearEnd, return_name=True)
+holidayDates = holidays.index
 remainingBDays = len(pd.DatetimeIndex(start=today,end=yearEnd, freq=us_bd))
 workable_days = remainingBDays
 days_off = ''
 
 @app.route('/', methods=['GET', 'POST'])
 def mainPage():
-    blankPage = render_template('main.html', days_off='', holidays='', workable_days='', checked=None)
     hours_worked = request.form.get('hours_worked') or request.cookies.get('hours_worked')
-    annual_target = request.form.get('annual_target') or request.cookies.get('annual_target')
-    hours_per_day = request.form.get('hours_per_day') or request.cookies.get('hours_per_day')
+    annual_target = request.form.get('annual_target') or request.cookies.get('annual_target') or 1790
+    hours_per_day = request.form.get('hours_per_day') or request.cookies.get('hours_per_day') or 8
     include_today = request.form.get('include_today')
+    blankPage = render_template('main.html', days_off='', holidays='', workable_days='', checked=None, annual_target=annual_target, hours_per_day=hours_per_day)
     if request.method == 'POST' and hours_worked and annual_target and hours_per_day:
         try:
             hours_worked = float(hours_worked)
@@ -35,7 +37,7 @@ def mainPage():
         daysLeft = (pyhLeft / hours_per_day)
         days_off = round(Decimal(remainingBDays - daysLeft - int(bool(include_today))), 2)
         resp = make_response(render_template('main.html', days_off=days_off,
-             holidays=holidays, workable_days=(workable_days - int(bool(include_today))), hours_worked=hours_worked,
+             holidays=len(holidays), workable_days=(workable_days - int(bool(include_today))), hours_worked=hours_worked,
              annual_target=annual_target, hours_per_day=hours_per_day, checked=include_today))
         resp.set_cookie('hours_worked', str(hours_worked))
         resp.set_cookie('annual_target', str(annual_target))
